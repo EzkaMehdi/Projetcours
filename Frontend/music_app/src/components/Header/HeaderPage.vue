abb1abbf42af09bc3edf526bef887c1d0ee4cfbe
AhmedDWIIM <91135844+AhmedDWIIM@users.noreply.github.com>
2024-07-02 01:20:06 +0200

    header footer

--- Frontend/music_app/src/components/Header/HeaderPage.vue
+++ Frontend/music_app/src/components/Header/HeaderPage.vue
@@ -1,57 +1,43 @@
 <template>
   <nav class="bg-white border-gray-200 dark:bg-gray-900">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="/" class="flex items-center justify-center">
-        <img src="../../assets/logo.png" style="height: 200px">
-      </a>
+      <!-- Button for toggling menu on small screens -->
       <button @click="toggleMenu" type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default" aria-expanded="false">
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M1 1h15M1 7h15M1 13h15" />
         </svg>
       </button>
-      <div :class="{'hidden': !isMenuOpen, 'block': isMenuOpen}" class="w-full md:block md:w-auto flex justify-center" id="navbar-default">
-        <ul
-          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li v-if="!isLoggedIn">
-            <button class="ButtonPrimary w-80 h-12 px-12 m-5 py-2.5 bg-violet-600 rounded-lg flex-col justify-center items-center gap-2.5 flex" @click="goToSignupPage">
-              <div class="Frame590 px-2 justify-center items-center gap-2.5 inline-flex">
-                <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                  S'inscrire
-                </span>
+
+      <!-- Single Div for the Logo and Buttons -->
+      <div class="w-full flex items-center justify-between">
+        <!-- Center the Logo -->
+        <div class="flex-1 flex justify-center">
+          <a href="/" class="flex items-center">
+            <img src="../../assets/logo.png" alt="Logo" class="h-16 w-auto">
+          </a>
+        </div>
+
+        <!-- Buttons aligned to the right -->
+        <div :class="{'hidden': !isMenuOpen, 'block': isMenuOpen}" class="w-full md:w-auto md:flex md:items-center md:ml-auto">
+          <ul class="font-medium flex flex-col md:flex-row p-4 md:p-0 mt-4 md:mt-0 border border-gray-100 md:border-0 rounded-lg bg-gray-50 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li v-if="!isLoggedIn" class="md:ml-4">
+              <button @click="goToSignupPage" class="ButtonPrimary">S'inscrire</button>
+            </li>
+            <li v-if="!isLoggedIn" class="md:ml-4">
+              <button @click="goToSigninPage" class="ButtonPrimary">Se connecter</button>
+            </li>
+            <li v-if="isLoggedIn" class="md:ml-4">
+              <button @click="logout" class="ButtonPrimary bg-red-600">Deconnexion</button>
+              <div v-if="isAdmin" class="md:ml-4">
+                <button @click="goToAdminHomePage" class="ButtonPrimary bg-red-600">Admin Page</button>
               </div>
-            </button>
-          </li>
-          <li v-if="!isLoggedIn">
-            <button class="ButtonPrimary w-80 h-12 px-12 m-5 py-2.5 bg-violet-600 rounded-lg flex-col justify-center items-center gap-2.5 flex" @click="goToSigninPage">
-              <div class="Frame590 px-2 justify-center items-center gap-2.5 inline-flex">
-                <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                  Se connecter
-                </span>
-              </div>
-            </button>
-          </li>
-          <li v-if="isLoggedIn">
-            <button @click="logout" class="ButtonPrimary w-80 h-12 px-12 m-5 py-2.5 bg-red-600 rounded-lg flex-col justify-center items-center gap-2.5 flex text-white">
-              Deconnexion
-            </button>
-            <div v-if="isAdmin">
-              <button @click="goToAdminHomePage" class="ButtonPrimary w-80 h-12 px-12 m-5 py-2.5 bg-red-600 rounded-lg flex-col justify-center items-center gap-2.5 flex text-white">
-                Admin Page
-              </button>
-            </div>
-            <button class="ButtonPrimary w-80 h-12 px-12 m-5 py-2.5 bg-violet-600 rounded-lg flex-col justify-center items-center gap-2.5 flex" @click="goToEventsPage">
-              <div class="Frame590 px-2 justify-center items-center gap-2.5 inline-flex">
-                <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                  Annonces
-                </span>
-              </div>
-            </button>
-          </li>
-        </ul>
+              <button @click="goToEventsPage" class="ButtonPrimary md:ml-4">Annonces</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
@@ -100,8 +86,6 @@ onMounted(() => {
 onMounted(async () => {
   try {
     const authToken = localStorage.getItem('authToken');
-    console.log('Token d\'authentification ici :', authToken);
-
     const { payload } = useJwt(authToken);
     const roleId = payload.value.role;
 
@@ -114,16 +98,12 @@ onMounted(async () => {
 });
 </script>
 
-<style>
+<style scoped>
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
 
 .ButtonPrimary {
-  @apply w-full md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center;
-}
-
-.ButtonSecondary {
-  @apply text-center text-white text-base font-bold font-['Roboto'];
+  @apply w-full md:w-40 h-10 px-6 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center text-white text-base font-bold font-['Inter'] m-2;
 }
 </style>
--- Frontend/music_app/src/views/HomeView.vue
+++ Frontend/music_app/src/views/HomeView.vue
@@ -1,73 +1,76 @@
 <template>
     <div>
-        <HeaderPage></HeaderPage>
-        <section class="flex flex-col md:flex-row justify-center items-center p-4 md:p-8">
-            <div class="text-center md:text-left md:mr-8">
-                <h1
-                    class="font-inter text-3xl md:text-5xl font-extrabold leading-normal tracking-normal text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mb-4">
-                    Les Meilleurs Evenements sont tout autour de vous !
-                </h1>
-                <h3 class="font-inter text-2xl md:text-3xl font-extrabold leading-normal tracking-normal">
-                    Proposez ou assistez aux meilleurs évenements proche de chez vous ou que vous soyez !
-                </h3>
-                <div class="flex flex-col md:flex-row mt-6">
-                    <button
-                        class="ButtonPrimary w-full md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
-                        @click="goToCityPage">
-                        <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                            Trouver un evenement
-                        </span>
-                    </button>
-                    <button
-                        class="ButtonPrimary w-full md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
-                        @click="goAddEventPage">
-                        <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                            Proposer un evenement
-                        </span>
-                    </button>
-                    <button
-                        class="ButtonPrimary w-full md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
-                        @click="goForumPage">
-                        <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
-                            Acceder au forum
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <img src="../assets/home.png" class="w-full md:w-1/2 mt-6 md:mt-0" alt="Home image">
-        </section>
+      <HeaderPage />
+      <section class="flex flex-col md:flex-row justify-center items-center p-4 md:p-16">
+        <div class="text-center md:text-left md:mr-8 flex-1">
+          <h1
+            class="font-inter text-3xl md:text-5xl font-extrabold leading-tight tracking-normal text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 mb-6">
+            Les Meilleurs Evenements sont tout autour de vous !
+          </h1>
+          <h3 class="font-inter text-2xl md:text-3xl font-extrabold leading-tight tracking-normal mb-6">
+            Proposez ou assistez aux meilleurs évenements proche de chez vous ou que vous soyez !
+          </h3>
+          <div class="flex flex-col md:flex-row justify-center md:justify-start">
+            <button
+              class="ButtonPrimary md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
+              @click="goToCityPage">
+              <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
+                Trouver un evenement
+              </span>
+            </button>
+            <button
+              class="ButtonPrimary md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
+              @click="goAddEventPage">
+              <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
+                Proposer un evenement
+              </span>
+            </button>
+            <button
+              class="ButtonPrimary md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center"
+              @click="goForumPage">
+              <span class="ButtonSecondary text-center text-white text-base font-bold font-['Roboto']">
+                Acceder au forum
+              </span>
+            </button>
+          </div>
+        </div>
+        <img src="../assets/home.png" class="w-full md:w-1/2 mt-6 md:mt-0 flex-1" alt="Home image">
+      </section>
+      <FooterPage />
     </div>
-</template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import HeaderPage from '../components/Header/HeaderPage.vue';
-import { useRouter } from 'vue-router';
-const router = useRouter();
-
-const goToCityPage = () => {
-    router.push({ path: "/city" })
-}
-
-const goAddEventPage = () => {
-    router.push({ path: "/add" })
-}
-
-const goForumPage = () => {
-    router.push({ path: "/forum" })
-}
-
-</script>
-<style>
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
-
-.ButtonPrimary {
-    @apply w-full md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center;
-}
-
-.ButtonSecondary {
+  </template>
+  
+  <script setup lang="ts">
+  import HeaderPage from '../components/Header/HeaderPage.vue';
+  import FooterPage from '../components/Footer/FooterPage.vue';
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+  const goToCityPage = () => {
+    router.push({ path: '/city' });
+  };
+  
+  const goAddEventPage = () => {
+    router.push({ path: '/add' });
+  };
+  
+  const goForumPage = () => {
+    router.push({ path: '/forum' });
+  };
+  </script>
+  
+  <style>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+  
+  .ButtonPrimary {
+    @apply md:w-80 h-12 px-6 m-2 py-2.5 bg-violet-600 rounded-lg flex justify-center items-center;
+  }
+  
+  .ButtonSecondary {
     @apply text-center text-white text-base font-bold font-['Roboto'];
-}
-</style>
+  }
+  </style>
+  
